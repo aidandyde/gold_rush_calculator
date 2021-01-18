ed9831110db1f398636b0579a5b4cfc1a8d2a1fd
Aidan Dyde <adyde@DESKTOP-5A990CS.lan>
2021-01-18 09:00:14 +0000

    made progress on the list display error

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const mysql = require('mysql');
 
+console.log("connecting DB");
+var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'rteb',
+        password : 'C0deH0riz0n!',
+        database : 'gold_rush_calculator',
+        insecureAuth : true
+    });
+    
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+app.get('/products_info', (req, res) => {
+  connection.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    if (result.length == 0 ) {
+  		res.send({ express: "database empty!"})
+    } else {
+  		res.send({ express: JSON.stringify(result) });
+  }
+  });
+  
+  
+  
 });
